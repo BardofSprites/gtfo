baf04474121d121176028d14da986e76ae0f71c2
Daniel <89086143+BardofSprites@users.noreply.github.com>
2024-11-15 11:00:13 -0500

    working copy files into structure

--- app/Main.hs
+++ app/Main.hs
@@ -3,15 +3,18 @@ module Main where
 import Data.Monoid
 import Data.Maybe
 import System.FilePath
+import System.Posix.Files
 import System.Directory
 import Control.Monad
+import Control.Exception (catch, SomeException)
 import qualified Data.Map as Map
 
 data FileType = Document
               | Picture
               | Video
+              | Archive
               | Unknown
-  deriving (Show, Eq, Ord)
+  deriving (Show, Eq, Ord, Enum, Bounded)
 
 type FileMap = Map.Map FileType [FilePath]
 
@@ -24,6 +27,10 @@ pictureExt = [".jpg", ".jpeg", ".xbm", ".bmp", ".png", ".gif"]
 videoExt :: [String]
 videoExt = [".mp4", ".avi", ".mov"]
 
+archiveExt :: [String]
+archiveExt = [".zip", ".tar.gz"]
+
+-- assigns type based on extension
 getFileType :: FilePath -> FileType
 getFileType path
     | ext `elem` documentExt = Document
@@ -33,6 +40,19 @@ getFileType path
   where
     ext = takeExtension path
 
+-- getAllFiles from a dir, excluding dirs, return list of files
+getAllFiles :: FilePath -> [FilePath] -> IO [FilePath]
+getAllFiles baseDir excludedDirs = do
+  entries <- listDirectory baseDir
+  let fullEntries = map (baseDir </>) entries
+  files <- filterM doesFileExist fullEntries
+  dirs <- filterM doesDirectoryExist fullEntries
+  let dirsToSearch = filter (\dir -> takeFileName dir `notElem` excludedDirs) dirs
+  nestedFiles <- forM dirsToSearch $ \dir -> getAllFiles dir excludedDirs
+  return (files ++ concat nestedFiles)
+
+-- classifyFilesMap: classify files into a map based on their type
+-- [(Document, [All the documents]), (Picture, [all the picture files]), etc...]
 classifyFilesMap :: [FilePath] -> FileMap
 classifyFilesMap = foldr insertFileType Map.empty
   where
@@ -40,42 +60,26 @@ classifyFilesMap = foldr insertFileType Map.empty
         let fileType = getFileType path
         in Map.insertWith (++) fileType [path] acc
 
-getAllFiles :: FilePath -> [FilePath] -> IO [FilePath]
-getAllFiles baseDir excludedDirs = do
-  entries <- listDirectory baseDir
+-- fileTypeFilter: filter files by file type from a dir, excluding dirs
+fileTypeFilter :: FilePath -> [FilePath] -> FileType -> IO [FilePath]
+fileTypeFilter dir excludedDirs fileType = do
+    files <- getAllFiles dir excludedDirs
+    return $ fromMaybe [] (Map.lookup fileType (classifyFilesMap files))
 
-  let fullEntries = map (baseDir </>) entries
-  files <- filterM doesFileExist fullEntries
-  dirs <- filterM doesDirectoryExist fullEntries
+copyFiles :: FileMap -> FilePath -> IO ()
+copyFiles fileMap destination = do
+  forM_ (Map.toList fileMap) $ \(fileType, files) -> do
+    let destDir = destination </> show fileType
+    createDirectoryIfMissing True destDir
+    forM_ files $ \file -> do
+      let destFile = destDir </> takeFileName file
+      copyFile file destFile
+      putStrLn $ "Copied " ++ file ++ " to " ++ destFile
 
-  let dirsToSearch = filter (\dir -> takeFileName dir `notElem` excludedDirs) dirs
-  nestedFiles <- forM dirsToSearch $ \dir -> getAllFiles dir excludedDirs
-  return (files ++ concat nestedFiles)
-
-classifyAllFilesInDirectory :: FilePath -> [FilePath] -> IO (FileMap)
-classifyAllFilesInDirectory baseDir excludedDirs = do
-  allFiles <- getAllFiles baseDir excludedDirs
-  let fileTypeMap = classifyFilesMap allFiles
-  return fileTypeMap
-
-filterFileType :: FileType -> FileMap -> Maybe [FilePath]
-filterFileType fileType fileMap = Map.lookup fileType fileMap
-
--- basically same as filterFileType, but nice printing
-prettyFileMap :: FilePath -> [FilePath] -> IO ()
-prettyFileMap baseDir excludedDirs = do
-  fileTypeMap <- classifyAllFilesInDirectory baseDir excludedDirs
-
-  mapM_ (\(fileType, files) -> do
-            putStrLn $ "FileType: " ++ show fileType
-            mapM_ putStrLn files
-        ) (Map.toList fileTypeMap)
-
-main :: IO ()
+main :: IO()
 main = do
-  let baseDir = "/home/bard/Pictures/wallpaper"
-  let excludedDirs = [".git" , "node_modules"]
-  -- fileTypeMap <- classifyAllFilesInDirectory baseDir excludedDirs
-  -- putStrLn $ show $ filterFileType Picture fileTypeMap
-
-  prettyFileMap baseDir excludedDirs
+  let baseDir = "/home/bard/testo-besto"
+  let excludedDirs = [".git", "node_modules"]
+  fileMap <- classifyFilesMap <$> getAllFiles baseDir excludedDirs
+  let destinationDir = "/home/bard/testo-even-bestoer"
+  copyFiles fileMap destinationDir
