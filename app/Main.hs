5df25d8b48f4868d597a8af8a9b8cc26aa2995d7
Daniel <89086143+BardofSprites@users.noreply.github.com>
2024-11-18 17:06:38 -0500

    inprogress: keywords + date

--- app/Main.hs
+++ app/Main.hs
@@ -17,6 +17,7 @@ data FileType = Document
   deriving (Show, Eq, Ord, Enum, Bounded)
 
 type FileMap = Map.Map FileType [FilePath]
+type Keyword = String
 
 documentExt :: [String]
 documentExt = [".pdf", ".docx", ".txt", ".org"]
@@ -75,6 +76,15 @@ copyFiles fileMap destination = do
       let destFile = destDir </> takeFileName file
       copyFile file destFile
       putStrLn $ "Copied " ++ file ++ " to " ++ destFile
+
+appendInfo :: FilePath -> [Keyword] -> IO FilePath
+appendInfo filePath keywords = do
+  currentTime <- getCurrentTime
+  let timestampStr = formatTime defaultTimeLocale "%Y%m%dT%H%M%S" currentTime
+      keywordsStr = intercalate "__" keywords
+      newFileName = timestampStr ++ "--" ++ keywordsStr ++ "--" ++ takeFileName filePath
+  return newFileName
+
 
 main :: IO()
 main = do
